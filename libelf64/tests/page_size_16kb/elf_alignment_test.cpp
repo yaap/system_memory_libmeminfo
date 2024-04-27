abd73eadf6afba6029cf2787cf3e9bb991a40c18
Steven Moreland <smoreland@google.com>
2024-04-27 00:03:41 +0000

    elf_alignment_test: specify VSR version
    Bug: 280101825
    Test: N/A
    Change-Id: Iad1206e5037fc4639b259bca9c2b16f9495ce1ae

--- libelf64/tests/page_size_16kb/elf_alignment_test.cpp
+++ libelf64/tests/page_size_16kb/elf_alignment_test.cpp
@@ -101,6 +101,7 @@ class ElfAlignmentTest :public ::testing::TestWithParam<std::string> {
     }
 };
 
+// @VsrTest = 3.14.1
 TEST_P(ElfAlignmentTest, VerifyLoadSegmentAlignment) {
   android::elf64::ForEachElf64FromDir(GetParam(), &LoadAlignmentCb);
 }
